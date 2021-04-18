49b7dbe95da49ae84107a0b0b53368836796b550
Hector Alejandro <79550176+hle778@users.noreply.github.com>
2021-04-18 19:57:36 -0400

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,11 +2,11 @@ module.exports = (function(settings) {
   console.log(settings["test_settings"]["default"]["username"]);
 
   if (process.env.LT_USERNAME) {
-    settings["test_settings"]["default"]["username"] = process.env.LT_USERNAME;
+    settings["test_settings"]["default"]["username"] = hecticodj;
   }
 
   if (process.env.LT_ACCESS_KEY) {
-    settings["test_settings"]["default"]["access_key"] = process.env.LT_ACCESS_KEY;
+    settings["test_settings"]["default"]["access_key"] = cTiWTW6JTl9hLPDPqf98nEX2xi58A2GG1KzaE0LwjXQ01Fn9wf;
   }
 
   if (process.env.SELENIUM_HOST) {
